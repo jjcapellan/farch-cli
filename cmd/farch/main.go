b02e91802259b41d14f85203e47a63f7a3e577d1
jjcapellan <soycape@hotmail.com>
2022-06-27 13:56:59 +0200

    Added: use of animated status

--- cmd/farch/main.go
+++ cmd/farch/main.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sync"
 
+	anim "github.com/jjcapellan/farch-cli/pkg/animation"
 	"github.com/jjcapellan/farch-cli/pkg/pass"
 	. "github.com/jjcapellan/jj-archiver"
 )
 
 var command, input, output, password string
+var wg sync.WaitGroup
 
 func main() {
 
@@ -29,59 +32,87 @@ func main() {
 	if command == "backup" {
 		err = backup(input, output, password)
 		if err != nil {
+			anim.StopAnim("An error has occurred")
+			wg.Wait()
 			showHelp()
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
+		anim.StopAnim("Backup tasks completed")
+		wg.Wait()
 	}
 
 	if command == "restore" {
 		err = restore(input, output, password)
 		if err != nil {
+			anim.StopAnim("An error has occurred")
+			wg.Wait()
 			showHelp()
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
+		anim.StopAnim("Restore tasks completed")
+		wg.Wait()
 	}
 }
 
 func backup(inputPath string, outputPath string, password string) error {
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		anim.AnimatedInfo("Packaging...[1/4]")
+	}()
+
 	packedData, err := PackFolder(inputPath)
 	if err != nil {
 		return err
 	}
 
+	anim.SetInfo("Compressing...[2/4]")
 	gzipData, err := Compress(packedData, "file.gzip")
 	if err != nil {
 		return err
 	}
 
+	anim.SetInfo("Encrypting...[3/4]")
 	encryptData, err := Encrypt(gzipData, password)
 	if err != nil {
 		return err
 	}
 
+	anim.SetInfo("Writing file...[4/4]")
 	err = WriteFile(outputPath, encryptData, 0666)
 
 	return err
 }
 
 func restore(inputPath string, outputPath string, password string) error {
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		anim.AnimatedInfo("Reading file...[1/4]")
+	}()
+
 	data, err := ReadFile(inputPath)
 	if err != nil {
 		return err
 	}
 
+	anim.SetInfo("Decrypting...[2/4]")
 	decryptData, err := Decrypt(data, password)
 	if err != nil {
 		return err
 	}
 
+	anim.SetInfo("Decompressing...[3/4]")
 	gzipData, _, err := Decompress(decryptData)
 	if err != nil {
 		return err
 	}
 
+	anim.SetInfo("Unpacking...[4/4]")
 	err = Unpack(gzipData, outputPath)
 
 	return err
